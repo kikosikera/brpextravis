12f0d55c17946031331604813611f1b9cfa20a36
San Budiman <sbudiman@rocketmail.com>
2018-03-25 02:41:16 -0700

    add install.packages("testthat") prior to loading the library -> to make fix for   > library(testthat)   Error in library(testthat) : there is no package called 'testthat'

--- tests/testpackage.R
+++ tests/testpackage.R
@@ -1,6 +1,8 @@
+install.packages("testthat")
 library(testthat)
 
-testthat::test_that("File name",{
+
+test_that("File name",{
     filename <- make_filename(2013)
-  testthat::expect_that(filename,equals("accident_2013.csv.bz2"))
+  expect_that(filename,equals("accident_2013.csv.bz2"))
 })
